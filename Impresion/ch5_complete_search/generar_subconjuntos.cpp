e1e0ebe4036840f229f163fb4883c42f4f87b337
matiasfernandez1 <matiasfernandeztaipe@gmail.com>
2019-12-15 18:50:19 -0300

    master

--- Impresion/ch5_complete_search/generar_subconjuntos.cpp
+++ Impresion/ch5_complete_search/generar_subconjuntos.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
 vector <int> subset;
-void search(int k) {
+void search(int k, int n) {
 if (k == n) {
-// process subset
+for(int i=0; i<subset.size(); i++){
+    cout<<subset[i]<<" ";
+}
+cout<<"\n";
 } else {
-search(k+1);
+search(k+1, n);
 subset.push_back(k);
-search(k+1);
+search(k+1, n);
 subset.pop_back();
 }
 }
 int main(){
+    int n;
     cin>>n;
-    search(n);
+    search(1, n+1);
     return 0;
 }
