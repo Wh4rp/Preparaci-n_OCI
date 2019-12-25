8a73667d9f1b98f90311366ba40f31685e09a653
matiasfernandez1 <matiasfernandeztaipe@gmail.com>
2019-12-24 21:51:27 -0300

    24/dic/2019

--- Problems/Codeforce/731A_Night_at_the_Museum.cpp
+++ Problems/Codeforce/731A_Night_at_the_Museum.cpp
@@ -5,12 +5,16 @@ int main(){
     cin.tie(NULL);
     cout.setf(ios::fixed);
     cout.precision(4);
-    int n;
+    int n, p=1, s=0;
     string v;
     cin>>v;
     for(int i=0; i<v.size(); i++){
+        n=v[i]-96;
+        
+        s+=min(abs(n-p), min(abs(26+n-p), abs(26-n+p)));
+        p=n;
         
     }
-    
+    cout<<s<<"\n";
 return 0;
 }
